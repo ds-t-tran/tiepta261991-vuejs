0d3052863cd2067dae881fc5ec807cbf860a617b
Tiep Tran <t-tran@dimage.co.jp>
2020-04-03 17:45:23 +0700

    components in vue js

--- src/app.js
+++ src/app.js
@@ -13,3 +13,89 @@ const app = new Vue({
 })
 
 export { app, router, store }
+
+Vue.component('child', {
+  props: ['message'],
+  template: '<h1>{{ message }}</h1>',
+  
+});
+var app = new Vue({
+  el: '#app',
+});
+
+Vue.component('child', {
+  props: ['mySay'],
+  template: '<h1>{{ mySay }}</h1>',
+
+});
+var app = new Vue({
+  el: '#app',
+})
+
+Vue.component('child', {
+  props: ['mySay'],
+  template: '<h1>{{ mySay }}</h1>',
+
+});
+var app = new Vue({
+  el: '#app',
+  data: {
+      message : null
+  }
+});
+
+Vue.component('child', {
+  props: ['mySay'],
+  template: '<h1>{{ typeof(mySay) }}</h1>',
+
+});
+var app = new Vue({
+  el: '#app',
+});
+
+Vue.component('child', {
+  props: ['mySay'],
+  data : function () {
+      return { dataSay : this.mySay + " This is dataSay!"};
+  },
+  template: '<h1>{{ dataSay }}</h1>',
+
+});
+var app = new Vue({
+  el: '#app',
+});
+
+Vue.component('child', {
+  props: ['mySay'],
+  computed : {
+      convertSay: function () {
+          return this.mySay.trim().toUpperCase();
+      }
+  },
+  template: '<h1>{{ convertSay }}</h1>',
+});
+var app = new Vue({
+  el: '#app'
+});
+
+Vue.component('ex',{
+  props: {
+      name: 'test',
+
+      class: [1, 'test'],
+      
+      point: {
+          type: 1
+      }
+  }
+});
+
+Vue.component('ex',{
+  props: {
+      point: {
+          validator: function (value) {
+              return value > 10;
+          }
+      }
+  }
+});
